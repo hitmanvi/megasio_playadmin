ca3ca133d27cb7cfe929747e91040da424e29068
hitman <hitmanvi@qq.com>
2025-10-17 17:19:20 +0800

    update

--- app/Models/Translation.php
+++ app/Models/Translation.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 class Translation extends Model
 {
-    protected $table = 'megasio_play_api.tag_translations';
+    protected $table = 'megasio_play_api.translations';
     /**
      * The attributes that are mass assignable.
      *
