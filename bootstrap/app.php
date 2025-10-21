c82d2b47cab5b3592f33b38715bfb63ebe42a387
hitman <hitmanvi@qq.com>
2025-10-21 10:18:15 +0800

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Enums\Err;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,15 +14,4 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->render(function (\Illuminate\Database\QueryException $e, $request) {
-            if (str_contains($e->getMessage(), 'database') && str_contains($e->getMessage(), 'not found')) {
-                $resp = [
-                    'code'   => Err::ACCOUNT_NOT_FOUND,
-                    'errmsg' => 'Not Found',
-                    'data'   => null,
-                ];
-        
-                return response()->json($resp);
-            }
-        });
     })->create();
