510e448625533b47f5ade1523a9354153ffaef9f
hitman <hitmanvi@qq.com>
2025-10-27 15:42:37 +0800

    update

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -2,12 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
 class Banner extends Model
 {
-    use HasFactory;
+    protected $table = 'megasio_play_api.banners';
 
     protected $fillable = [
         'type',
