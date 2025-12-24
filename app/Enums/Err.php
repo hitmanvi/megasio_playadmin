df64d8dfa3c1a7489021f8fbf8ff004a65babc52
hitman <hitmanvi@qq.com>
2025-12-24 14:38:10 +0800

    update

--- app/Enums/Err.php
+++ app/Enums/Err.php
@@ -4,12 +4,5 @@ namespace App\Enums;
 
 class Err
 {
-    const SUCCESS = ['200', 'success'];
-    const INVALID_PARAMS = ['422', 'invalid params'];
-    const ACCESS_DENIED = ['403', 'access denied'];
-    const ACCOUNT_NOT_FOUND = ['404', 'account not found'];
-    const ERROR = ['500', 'error'];
-    const RECORD_ALREADY_EXISTS = ['400', 'record already exists'];
-
-    const SOPAY_ERROR = ['500', 'sopay error'];
+    const SOPAY_ERROR = [0, 'sopay error'];
 }
