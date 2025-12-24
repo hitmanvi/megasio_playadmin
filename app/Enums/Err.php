b581a8740d36de6e6218a4275b609c3850a46a19
hitman <hitmanvi@qq.com>
2025-12-24 14:51:28 +0800

    update

--- app/Enums/Err.php
+++ app/Enums/Err.php
@@ -4,6 +4,9 @@ namespace App\Enums;
 
 class Err
 {
+    const INVALID_PARAMS = [422, 'invalid params'];
+    const ERROR = [500, 'error'];
+    
     const UNKNOWN_ERROR = [10000, 'unknown error'];
     const SOPAY_ERROR = [10001, 'sopay error'];
 }
